a87d06a41a0d41b9b60159935aafb7e590902e07
koch-econ <43249790+koch-econ@users.noreply.github.com>
2022-04-15 15:31:19 +0300

    Update repl_path.py
    Script repl_path.py to update pathes in ...inport.sql
    
    add print() to report status and simple error handling

--- repl_path.py
+++ repl_path.py
@@ -2,17 +2,24 @@
 ## with script folder path and csv subdirectory 
 
 import os 
+import sys
 
 base_dir_path=os.path.dirname(os.path.abspath(__file__))
 
-print(base_dir_path)
+# print(base_dir_path)
 new_path="/".join(base_dir_path.split("\\"))
 old_path='/path/to'
 file_name:str="/".join([new_path,"lahman_2016_import.sql"])
 csv_name:str="/".join([new_path,"csv"])
-with open(file_name) as in_file:
-    text_py:str=in_file.read()
-new_text_py= text_py.replace(old_path,csv_name)
-print(new_text_py)
-with open(file_name,"w") as out_file:
-    text_py:str=out_file.write(new_text_py)
+try:
+    with open(file_name) as in_file:
+        text_py:str=in_file.read()
+    new_text_py= text_py.replace(old_path,csv_name)
+    # print(new_text_py)
+    with open(file_name,"w") as out_file:
+        out_file.write(new_text_py)
+  
+    print("Paths in", file_name , "were replaced")
+except Exception as e:
+    print("Error occured. Import script not ready to run.")
+    print("error: ",e)
